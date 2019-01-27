2a28361b4a9a552001b5816d189dda94891b1305
Marius Craciunoiu <marius@craciunoiu.net>
2019-01-26 18:20:38 -0700

    Disable annoying eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/prefer-stateless-function': 0,
+    'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 }
