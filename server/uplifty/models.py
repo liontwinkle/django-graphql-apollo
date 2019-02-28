4cc65b89fbf8d3af11150fa7ca52aafa2edd59ad
liontwinkle <lion.twinkle.ge@gmail.com>
2019-02-28 16:24:30 +0100

    Add slug field

--- server/uplifty/models.py
+++ server/uplifty/models.py
@@ -11,6 +11,12 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        """Add ordering."""
+
+        ordering = ["-created_at"]
